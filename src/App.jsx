354cb5e33d827c96edd3dec04efb5ab33bc5be2c
Matin-Hosseini <matinpinterest2003@gmail.com>
2023-11-10 18:47:18 +0330

    hero and companies created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Companies from "./Components/Companies/Companies";
 import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero/>
+      <Companies/>
     </>
   );
 }
