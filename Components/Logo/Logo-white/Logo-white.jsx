3d0b8fb43d0ebbf495f31e6999007bea9d83325e
Matin-Hosseini <Matin-Hosseini@users.noreply.github.com>
2023-11-11 07:52:59 +0000

    deploy: 50630299e0115b290d66611670d3b1036274dc43

--- Components/Logo/Logo-white/Logo-white.jsx
+++
@@ -1,9 +0,0 @@
-import "./LogoWhtie.css"
-
-export default function LogoWhite(){
-    return(
-        <div className="logo-white">
-            <img src="src/assets/images/logo/logo-white.svg" alt="" />
-        </div>
-    )
-}
