b52e1453f79464b33764dd49685843ff362296e3
Matin-Hosseini <matinpinterest2003@gmail.com>
2023-11-11 09:18:53 +0330

    funfacts added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Components/About/About";
 import Blogs from "./Components/Blogs/Blogs";
 import Companies from "./Components/Companies/Companies";
+import FunFacts from "./Components/FunFacts/FunFacts";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import Testimonial from "./Components/Testimonial/Testimonials";
@@ -15,6 +16,7 @@ function App() {
       <About/>
       <Testimonial/>
       <Blogs/>
+      <FunFacts/>
     </>
   );
 }
